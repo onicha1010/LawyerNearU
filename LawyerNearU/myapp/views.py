bad7ffc93c8cb5f75fcff9033abab7aa5b4cbc0a
Louisrelax <112146664+Louisrelax@users.noreply.github.com>
2024-01-22 01:36:27 +0700

    add template and css

--- LawyerNearU/myapp/views.py
+++ LawyerNearU/myapp/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Home</h1>")
+    return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse("<h1>About</h1>")
+    return render(request, 'about.html')
 
 def form(request):
     return HttpResponse("<h1>Form</h1>")
